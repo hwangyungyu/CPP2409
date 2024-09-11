2923fa18a7eb0e01e13233d90b155376cc40d8e9
hwangyungyu <vlzkcbdmdkr@naver.com>
2024-09-11 16:33:44 +0900

    In/Out Function and Operator Exercises

--- challenge/week2/temperature.cpp
+++ challenge/week2/temperature.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main(){
     int C;
-    float F;
+    float F, q;
     cout << "화씨온도를 입력해주세요: ";
     cin >> C;
 
-    cout << "최대로 살 수 있는 캔디: " << (5.0/9.0)*(C - 32) << endl;
+    q = (5.0/9.0)*(C - 32);
+
+    cout << "섭씨 온도: " << q << endl;
 
     return 0;
 }
