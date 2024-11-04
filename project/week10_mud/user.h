9335060cde56ce2661281a8705ce61e4fb5c40fb
hwangyungyu <vlzkcbdmdkr@naver.com>
2024-11-04 16:18:49 +0900

    Constructor & Destructor Exercises

---
+++ project/week10_mud/user.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class User{
+private:
+    int hp;
+public:
+    User();
+    void IncreaseHP(int inc_hp);
+    void DecreaseHP(int dec_hp);
+    int GetHP();
+};
